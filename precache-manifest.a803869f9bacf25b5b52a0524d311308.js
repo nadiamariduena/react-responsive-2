794e283f854d3051bf1c334ccc520363ae4fb835
melissa <nadia.mm.mariduena@gmail.com>
2020-09-10 22:50:40 +0200

    Updates

---
+++ precache-manifest.a803869f9bacf25b5b52a0524d311308.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "85746e84aac51aba60c3c0d8636dc070",
+    "url": "/react-responsive-2/index.html"
+  },
+  {
+    "revision": "edb6259beb037eee80c4",
+    "url": "/react-responsive-2/static/css/main.23419f16.chunk.css"
+  },
+  {
+    "revision": "f171c9bd38e45acac746",
+    "url": "/react-responsive-2/static/js/2.bfa32345.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-responsive-2/static/js/2.bfa32345.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "edb6259beb037eee80c4",
+    "url": "/react-responsive-2/static/js/main.91c3f9fd.chunk.js"
+  },
+  {
+    "revision": "24bb23ecc8ad9e957e4b",
+    "url": "/react-responsive-2/static/js/runtime-main.3a672dcf.js"
+  }
+]);
